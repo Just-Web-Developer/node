49a98ac4f40e55476635ddd28c76c1b67524ff18
Vladyslav Smolianets <vsmolianets@stalker2.com>
2022-01-17 19:53:16 +0200

    First try

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+app.get('*', cors(corsOptions), function (req, res, next) {
     res.json({msg: 'This is CORS-enabled for only https://pedantic-hopper-f5b19a.netlify.app/.'})
 })
 
